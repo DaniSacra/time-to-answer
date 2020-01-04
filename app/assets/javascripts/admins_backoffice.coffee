43bd5aeb14ec3394bf8bd9ec37d94e95b72d8010
Daniel Sacramento <danisacra@gmail.com>
2020-01-04 17:03:28 -0300

    added CRUD for admins

--- app/assets/javascripts/admins_backoffice.coffee
+++ app/assets/javascripts/admins_backoffice.coffee
@@ -5,3 +5,4 @@
 //= require bootstrap/dist/js/bootstrap.bundle.min
 //= require jquery.easing/jquery.easing.min
 //= require sb-admin-2.min
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,42 +1,57 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
-  
-  before_action :set_admin, only: [:edit, :update]
-  before_action :admin_params, only: [:update]
   before_action :verify_password, only: [:update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
     @admins = Admin.all.order(:id)
   end
 
-  def edit
-    #before_action
+  def new
+    @admin = Admin.new
   end
 
-  def update
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
 
+  def edit
+  end
+
+  def update    
     if @admin.update(admin_params)
-      redirect_to admins_backoffice_admins_path, notice: "Administrador salvo com sucesso"
+#      AdminMailer.update_email(current_admin, @admin).deliver_now
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
     end
+  end
 
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Administrador excluído com sucesso!"
+    else
+      render :index
+    end
   end
 
   private
+  
+  def admin_params
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
 
-    def set_admin
-      @admin = Admin.find(params[:id])  
-    end
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
 
-    def admin_params
-      params.require(:admin).permit(:email, :password, :password_confirmation)
+  def verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
     end
-    
-    #extract passwords from params
-    def verify_password
-      if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-        params.extract!(:password, :password_confirmation)
-      end  
-    end
-    
+  end
 end
